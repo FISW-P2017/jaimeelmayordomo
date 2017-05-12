80acb46175b8b363802079bd13dd7e5aeba37225
alejandro mora <alex-mora95@hotmail.com>
2017-05-12 08:00:12 -0500

    JEM003: Agregar tarjeta de credito

--- JaimeElMayordomo/app/src/main/java/com/iteso/jaimeelmayordomo/MainActivity.java
+++ JaimeElMayordomo/app/src/main/java/com/iteso/jaimeelmayordomo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.iteso.jaimeelmayordomo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        changeTextOnce();
+
     }
+
+
+
+
+
+    public void changeTextOnce(){
+        Button changeTextButton = (Button) findViewById(R.id.button2);
+        final TextView changingText = (TextView) findViewById(R.id.editText);
+        final TextView changingText2 = (TextView) findViewById(R.id.editText2);
+        final TextView changingText3 = (TextView) findViewById(R.id.editText3);
+        final TextView changingText4 = (TextView) findViewById(R.id.editText4);
+
+
+        changeTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View t) {
+                changingText.setText("Número de la tarjeta: ");
+                changingText2.setText("Nombre del titular: ");
+                changingText3.setText("Fecha de vencimiento: ");
+                changingText4.setText("Número de seguridad: ");
+
+            }
+        });
+    }
+
+
 }
+
